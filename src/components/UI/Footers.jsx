6b4bdd6e8fb03602122473cbcbe64e081b240eb9
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-09-30 18:48:20 +0530

    country-section

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,43 +1,43 @@
 import footerContact from "../../api/footerApi.json"
-import { IoCallSharp } from "react-icons/io5";
-import { MdPlace } from "react-icons/md";
-import { TbMailPlus } from "react-icons/tb";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom"
 
-export const Footers=()=>{
+import { MdPlace } from "react-icons/md"
+import { IoCallSharp } from "react-icons/io5"
+import { TbMailPlus } from "react-icons/tb"
+export const Footers = () => {
 
-    const footerIcon={
-        MdPlace:<MdPlace/>,
-        IoCallSharp:<IoCallSharp/>,
-        TbMailPlus:<TbMailPlus/>
-    }
-    return (
-        <footer className="footer-section">
-            <div className="container grid grid-three-cols">
-                {
-                    footerContact.map((curData,index)=>{
-                        const{icon,title,details}=curData;
-                        return (
-                            <div className="footer-contact" key={index}>
-                                <div className="icon">{footerIcon[icon]}</div>
-                                <div className="footer-contact-text">
-                                    <p>{title}</p>
-                                    <p>{details}</p>
-                                </div>
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus />
+  }
+  return (
+    <footer className="footer-section">
+      <div className="container grid grid-three-cols">
+        {
+          footerContact.map((curData, index) => {
+            const { icon, title, details } = curData;
+            return (
+              <div className="footer-contact" key={index}>
+                <div className="icon">{footerIcon[icon]}</div>
+                <div className="footer-contact-text">
+                  <p>{title}</p>
+                  <p>{details}</p>
+                </div>
 
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="copyright-area">
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="copyright-area">
         <div className="container">
           <div className="grid grid-two-cols">
             <div className="copyright-text">
               <p>
                 Copyright &copy; 2025, All Right Reserved
                 <NavLink to="" target="_blank">
-                 Shubham
+                  Shubham
                 </NavLink>
               </p>
             </div>
@@ -59,8 +59,7 @@ export const Footers=()=>{
                 <li>
                   <NavLink
                     to="git@github.com:Shubh2301/Accordion.git"
-                    target="_blank"
-                  >
+                    target="_blank">
                     Source Code
                   </NavLink>
                 </li>
@@ -73,6 +72,6 @@ export const Footers=()=>{
         </div>
       </div>
 
-        </footer>
-    )
+    </footer>
+  )
 }
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState, useTransition } from "react"
+import { getCountryData } from "../api/postApi";
+import { Loader } from "../components/UI/Loader";
+import { CountryCard } from "../components/Layout/CountryCard";
+
 export const Country = () => {
+
+const [isPending,startTransition]=useTransition();
+const[countries,setCountries]=useState([]);
+
+  useEffect(()=>{
+    startTransition(async()=>{
+    const res =await getCountryData();
+    setCountries(res.data)
+    })
+
+  },[])
+
+  if(isPending) return <Loader/>;
+
   return (
-    <div>Country</div>
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {
+          countries.map((Curcountry,index)=>{
+            return <CountryCard key={index} country={Curcountry}/>
+          })
+
+        }
+      </ul>
+    </section>
   )
 }
